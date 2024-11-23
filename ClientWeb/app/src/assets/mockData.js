d7ad47df0d72e889a63a2ee52b96dda4401bd16a
Pichawat <pichawat43695@gmail.com>
2024-11-23 21:53:38 +0700

    Mock order data

---
+++ ClientWeb/app/src/assets/mockData.js
@@ -0,0 +1,179 @@
+export const mockOrders = [
+    {
+        "id": 1,
+        "status": "Cancelled",
+        "deliveryDate": null,
+        "items": [
+          {
+            "id": 11,
+            "name": "Rainforest Colors",
+            "image": "/api/placeholder/48/48"
+          }
+        ]
+      },
+      {
+        "id": 2,
+        "status": "Pending",
+        "deliveryDate": null,
+        "items": [
+          {
+            "id": 21,
+            "name": "Rainforest Colors",
+            "image": "/api/placeholder/48/48"
+          },
+          {
+            "id": 22,
+            "name": "Different Kinds of Dead and Other Tales",
+            "image": "/api/placeholder/48/48"
+          },
+          {
+            "id": 23,
+            "name": "Busy Beavers",
+            "image": "/api/placeholder/48/48"
+          }
+        ]
+      },
+      {
+        "id": 3,
+        "status": "Delivered",
+        "deliveryDate": "15 December, 2016",
+        "items": [
+          {
+            "id": 31,
+            "name": "Various Kinds of Japanese Flowers",
+            "image": "/api/placeholder/48/48",
+            "deliveryTime": "Arrives tomorrow",
+            "timeWindow": "7pm - 9pm"
+          }
+        ]
+      },
+      {
+        "id": 4,
+        "status": "Pending",
+        "deliveryDate": null,
+        "items": [
+          {
+            "id": 41,
+            "name": "The Candy-maker: A Practical Guide",
+            "image": "/api/placeholder/48/48"
+          },
+          {
+            "id": 42,
+            "name": "Animal Pack",
+            "image": "/api/placeholder/48/48"
+          }
+        ]
+      },
+      {
+        "id": 5,
+        "status": "Cancelled",
+        "deliveryDate": null,
+        "items": [
+          {
+            "id": 51,
+            "name": "Rainforest Colors",
+            "image": "/api/placeholder/48/48"
+          }
+        ]
+      },
+      {
+        "id": 6,
+        "status": "Pending",
+        "deliveryDate": null,
+        "items": [
+          {
+            "id": 61,
+            "name": "The Anatomy of Bibliomania",
+            "image": "/api/placeholder/48/48"
+          },
+          {
+            "id": 62,
+            "name": "The Anatomy of Bibliomania",
+            "image": "/api/placeholder/48/48"
+          }
+        ]
+      },
+      {
+        "id": 7,
+        "status": "Pending",
+        "deliveryDate": null,
+        "items": [
+          {
+            "id": 71,
+            "name": "Animal Pack",
+            "image": "/api/placeholder/48/48"
+          },
+          {
+            "id": 72,
+            "name": "The Candy-maker: A Practical Guide",
+            "image": "/api/placeholder/48/48"
+          }
+        ]
+      },
+      {
+        "id": 8,
+        "status": "Delivered",
+        "deliveryDate": "Tomorrow",
+        "items": [
+          {
+            "id": 81,
+            "name": "The Candy-maker: A Practical Guide",
+            "image": "/api/placeholder/48/48",
+            "deliveryTime": "Arrives tomorrow",
+            "timeWindow": "7pm - 9pm"
+          },
+          {
+            "id": 82,
+            "name": "Animal Pack",
+            "image": "/api/placeholder/48/48",
+            "deliveryTime": "Arrives tomorrow",
+            "timeWindow": "7pm - 9pm"
+          },
+          {
+            "id": 83,
+            "name": "Who Lives in a Tree?",
+            "image": "/api/placeholder/48/48",
+            "deliveryTime": "Arrives tomorrow",
+            "timeWindow": "7pm - 9pm"
+          }
+        ]
+      },
+      {
+        "id": 9,
+        "status": "Pending",
+        "deliveryDate": null,
+        "items": [
+          {
+            "id": 91,
+            "name": "The Anatomy of Bibliomania",
+            "image": "/api/placeholder/48/48"
+          },
+          {
+            "id": 92,
+            "name": "Different Kinds of Dead and Other Tales",
+            "image": "/api/placeholder/48/48"
+          }
+        ]
+      },
+      {
+        "id": 10,
+        "status": "Cancelled",
+        "deliveryDate": null,
+        "items": [
+          {
+            "id": 101,
+            "name": "The Anatomy of Bibliomania",
+            "image": "/api/placeholder/48/48"
+          },
+          {
+            "id": 102,
+            "name": "Supermarket",
+            "image": "/api/placeholder/48/48"
+          },
+          {
+            "id": 103,
+            "name": "Rainforest Colors",
+            "image": "/api/placeholder/48/48"
+          }
+        ]
+      }];
