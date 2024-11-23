7d48e9203f67c7995e8f858d9568ff402ff7dc77
Pichawat <pichawat43695@gmail.com>
2024-11-23 21:55:52 +0700

    Fix missing declaration on setIsSideNavOpen

--- ClientWeb/app/src/pages/Orders.jsx
+++ ClientWeb/app/src/pages/Orders.jsx
@@ -7,6 +7,7 @@ import { mockOrders } from '../assets/mockData.js';
 import { useTheme } from '../components/ThemeProvider';
 
 const OrderHistory = () => {
+  const [setIsSideNavOpen] = useState(false);
   const { theme } = useTheme();
 
   return (
